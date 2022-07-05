b87228b54f66c632b1a3ffdbe2d31d5b20f23fbf
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-07-05 11:05:02 -0400

    error fixes

--- public/service-worker.js
+++ public/service-worker.js
@@ -18,11 +18,11 @@ const FILES_TO_CACHE = [
     '/api/transaction'
 ]
 
-//install the service worker in browser
+//install service worker in browser
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            
+            console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
@@ -31,18 +31,18 @@ self.addEventListener('install', function (e) {
 //activate service worker
 self.addEventListener('activate', function(e) {
     e.waitUntil(
-    
       caches.keys().then(function(keyList) {
         let cacheKeeplist = keyList.filter(function(key) {
           return key.indexOf(APP_PREFIX);
         });
-        cacheKeeplist.push(CACHE_NAME); 
-
+        cacheKeeplist.push(CACHE_NAME);
+  
         
+        //delete old cache
         return Promise.all(
           keyList.map(function(key, i) {
             if (cacheKeeplist.indexOf(key) === -1) {
-              
+              console.log('deleting cache : ' + keyList[i]);
               return caches.delete(keyList[i]);
             }
           })
@@ -51,13 +51,15 @@ self.addEventListener('activate', function(e) {
     );
   });
 
-  
   self.addEventListener('fetch', function (e) {
+      console.log('fetch request : ' + e.request.url)
       e.respondWith(
           caches.match(e.request).then(function (request) {
               if (request) {
+                  console.log('responding with cache: ' + e.request.url);
                   return request;
               } else {
+                  console.log('file is not cached, fetching: ' + e.request.url);
                   return fetch(e.request);
               }
           })
