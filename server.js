971981042d865cb044ac05ac3b073853753c24de
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-08-17 16:34:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget_pwa";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/transaction";
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: true,
